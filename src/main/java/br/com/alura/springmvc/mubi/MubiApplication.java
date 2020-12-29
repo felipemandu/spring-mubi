df32759d74c0fec59b7bfc7b1fcf0cbea5bd01c3
Felipe Mandu Pereira <felipemandupereira@outlook.com>
2020-12-29 02:19:09 -0300

    Initial commit projeto de estudo | web app - spring thymeleaf botstrap

---
+++ src/main/java/br/com/alura/springmvc/mubi/MubiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.springmvc.mubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MubiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MubiApplication.class, args);
+	}
+
+}
