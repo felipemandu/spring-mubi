c655b50531fff18be17a790a99e7e90566ed26a3
Felipe Mandu Pereira <felipemandupereira@outlook.com>
2020-12-29 18:59:14 -0300

    Criação de endPoint por status e alteração de status na home.html

--- src/main/java/br/com/alura/springmvc/mubi/controller/HomeController.java
+++ src/main/java/br/com/alura/springmvc/mubi/controller/HomeController.java
@@ -1,22 +1,43 @@
 package br.com.alura.springmvc.mubi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.springmvc.mubi.domain.Pedido;
+import br.com.alura.springmvc.mubi.domain.StatusPedido;
 import br.com.alura.springmvc.mubi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String hello(Model model) {
 		model.addAttribute("pedidos", pedidoRepository.findAll());
 		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String status(@PathVariable("status") String status, Model model) {
+		StatusPedido statusPedido = Enum.valueOf(StatusPedido.class, status.toUpperCase());
+		List<Pedido> pedidos = pedidoRepository.findByStatus(statusPedido);
+		model.addAttribute("pedidos", pedidos);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 
 }
--- src/main/java/br/com/alura/springmvc/mubi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/springmvc/mubi/repository/PedidoRepository.java
@@ -1,10 +1,15 @@
 package br.com.alura.springmvc.mubi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.springmvc.mubi.domain.Pedido;
+import br.com.alura.springmvc.mubi.domain.StatusPedido;
 @Repository
 public interface PedidoRepository  extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedido statusPedido);
 
 }
